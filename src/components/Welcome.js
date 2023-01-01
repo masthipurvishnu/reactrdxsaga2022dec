0d13ebe05b30309ae41a439c869a6ba6c337fef5
Vishnu <masthipurvishnu@gmail.com>
2022-12-31 18:34:10 -0600

    React.memo

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,14 +1,14 @@
 import { PropTypes } from "prop-types";
 import React from "react";
 
-const Welcome = (props) => {
+const Welcome = React.memo((props) => {
     console.log('Welcome component rendered');
     return (
         <>
             <h1>Welcome to {props?.name} component..!</h1>
         </>
     )
-}
+})
 
 Welcome.propTypes = {
     name: PropTypes.string.isRequired
